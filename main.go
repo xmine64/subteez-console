4b9ae9940aaa244f9d623bd4a7021bc609ff2a3a
Mohammad Amin Mollazadeh <madamin@pm.me>
2021-11-24 15:43:45 +0330

    Load config on startup, or generate a default one

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"subteez/config"
+	"subteez/subteez"
+)
+
 func main() {
+	appConfig := config.NewConfigFile("subteez.config")
+	if appConfig.Load() != nil {
+		appConfig.SetServer("https://subteez1.herokuapp.com")
+		appConfig.SetLanguageFilters(subteez.Languages)
+		if appConfig.Save() != nil {
+			fmt.Println("Note: Error in saving default config")
+		}
+	}
 }
