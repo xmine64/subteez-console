f659f3417a199764561f9495a43187bc570792a5
Mohammad Amin Mollazadeh <madamin@pm.me>
2021-12-01 17:33:05 +0330

    Update help messages

--- messages/help.go
+++ messages/help.go
@@ -11,21 +11,22 @@ Options:
 
     -interactive    : Enable interactive mode
 
+    -script         : Enable script mode
+                      Script mode disables log and formats output for using in scripts
+
 Commands:
 	
 `
 	CommandRow = "    %s\t: %s\n\n"
 
-	SearchHelpTopic = `Usage: %s [-interactive] search <Query>
+	SearchHelpTopic = `Usage: %s [-interactive] [-script] search <Query>
 
-Search movies or series for given query and print the result.
-In interactive mode you can select a title using arrow keys.
+Search movies or series for given query and print the result or show a menu on interactive mode.
 
 `
-	FilesHelpTopic = `Usage: %s [-interactive] files <Movie ID>
+	FilesHelpTopic = `Usage: %s [-interactive] [-script] files <Movie ID>
 	
-Show available files for given movie or series ID.
-In interactive mode you can select a file using arrow keys.
+Show available files for given movie or series ID and print the result or show a menu on interactive mode.
 
 Example: files arcane-league-of-legends-first-season
 
@@ -35,7 +36,7 @@ Example using full address:
 `
 	DownloadHelpTopic = `Usage: %s download <Movie ID> <Language> <File ID>
 	
-Download file with specified file ID. Interactive mode is not supported.
+Download file with specified file ID and save it in a file, or write output in stdout on script mode.
 
 Examples: download arcane-league-of-legends-first-season fa 2625907
           download arcane-league-of-legends-first-season Persian 2625907
